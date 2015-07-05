6866aa716c1b213dec20b55ddc42bfd7f5daf265
Rakovskiy <rakovskiy28@mail.ru>
2015-07-05 23:54:34 +0300

    Перенёс модули и темы в common

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
+Yii::setAlias('urlFiles', '/files');
+Yii::setAlias('files', dirname(__DIR__) . '/files');
 Yii::setAlias('root', dirname(dirname(__DIR__)) . '/');
 Yii::setAlias('common', dirname(__DIR__));
-Yii::setAlias('files', dirname(__DIR__) . '/files');
-Yii::setAlias('urlFiles', '/files');
-Yii::setAlias('modules', dirname(dirname(__DIR__)) . '/modules');
-Yii::setAlias('themes', dirname(dirname(__DIR__)) . '/themes');
+Yii::setAlias('modules', dirname(__DIR__) . '/modules');
+Yii::setAlias('themes', dirname(__DIR__) . '/themes');
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
